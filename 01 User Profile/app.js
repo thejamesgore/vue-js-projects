954025e12d2bfdba5f39056e676d306f18b31798
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-05-15 19:19:47 +0100

    Using v bind directive

--- 01 User Profile/app.js
+++ 01 User Profile/app.js
@@ -2,6 +2,10 @@ const app = Vue.createApp({
   data() {
     return {
       firstName: 'James',
+      lastName: 'Jones',
+      email: 'J.Jones@Jonsing.com',
+      gender: 'male',
+      photo: 'https://randomuser.me/api/portraits/men/11.jpg',
     }
   },
 })
