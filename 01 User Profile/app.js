5a4ab05c3ecfec527ff485fbf6e7cac6182590c7
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-05-16 06:35:00 +0100

    using methods

--- 01 User Profile/app.js
+++ 01 User Profile/app.js
@@ -8,6 +8,15 @@ const app = Vue.createApp({
       photo: 'https://randomuser.me/api/portraits/men/11.jpg',
     }
   },
+  methods: {
+    getUser() {
+      ;(this.firstName = 'Hannah'),
+        (this.lastName = 'Bones'),
+        (this.email = 'hannah.bones"gmail.com'),
+        (this.gender = 'female'),
+        (this.photo = 'https://randomuser.me/api/portraits/women/11.jpg')
+    },
+  },
 })
 
 app.mount('#app')
