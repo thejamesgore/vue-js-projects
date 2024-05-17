1147f1a74bb95a5f5f34af9a357086b7a3ba5b52
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-05-17 08:17:09 +0100

    conditional styling using v-bind and ternary in array

--- 02 Task Tracker/src/components/Tasks.vue
+++ 02 Task Tracker/src/components/Tasks.vue
@@ -1,15 +1,19 @@
 <template>
-    <div v-for="task in tasks">
-        <h3>{{ task.text }}</h3>
+    <div :key="task.id" v-for="task in tasks">
+        <Task :task="task" />
     </div>
 </template>
 
 <script>
+import Task from './Task.vue'
 
 export default {
     name: 'Tasks',
     props: {
         tasks: Array
+    },
+    components: {
+        Task
     }
 }
 </script>
