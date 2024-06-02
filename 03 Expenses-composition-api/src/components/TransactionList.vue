96861854c2daa6644e2c8d38c4d8b82c15d43c93
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-06-02 19:54:45 +0100

    using setup function

--- 03 Expenses-composition-api/src/components/TransactionList.vue
+++ 03 Expenses-composition-api/src/components/TransactionList.vue
@@ -1,31 +1,28 @@
 <template>
-     <h3>History</h3>
-          <ul id="list" class="list">
-           <li 
-              v-for="transaction in transactions" 
-              v-bind:key="transaction.id" 
-              :class="transaction.amount < 0 ? 'minus' : 'plus'">
-            <p>{{ transaction.text }}</p> 
-            <span >£{{ transaction.amount }}</span>
-            <button class="delete-btn">x</button> 
-          </li>
-          </ul>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+      {{ transaction.text }} <span>${{ transaction.amount }}</span><button class="delete-btn"
+        @click="deleteTransaction(transaction.id)">
+        x
+      </button>
+    </li>
+  </ul>
 </template>
 
+<script setup>
+import { defineProps } from 'vue'
 
-<script>
-export default {
-  setup() {
-      const transactions = [
-        {id: 1, text: 'Flowers', amount: -19.99},
-        {id: 2, text: 'Salary', amount: 1300},
-        {id: 3, text: 'Book', amount: -10},
-        {id: 4, text: 'Camera', amount: -399},
-      ]
-  
-      return {
-        transactions
-      }
-  }
-}
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+})
+
+// const emit = defineEmits(['transactionDeleted'])
+
+// const deleteTransaction = (id) => {
+//   emit('transactionDeleted', id)
+// }
 </script>
