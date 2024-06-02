388233e1d2df11c4361ae05dbc3a8b07a3ae8c4f
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-06-02 19:49:22 +0100

    TransactionList

---
+++ 03 Expenses-composition-api/src/components/TransactionList.vue
@@ -0,0 +1,31 @@
+<template>
+     <h3>History</h3>
+          <ul id="list" class="list">
+           <li 
+              v-for="transaction in transactions" 
+              v-bind:key="transaction.id" 
+              :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            <p>{{ transaction.text }}</p> 
+            <span >£{{ transaction.amount }}</span>
+            <button class="delete-btn">x</button> 
+          </li>
+          </ul>
+</template>
+
+
+<script>
+export default {
+  setup() {
+      const transactions = [
+        {id: 1, text: 'Flowers', amount: -19.99},
+        {id: 2, text: 'Salary', amount: 1300},
+        {id: 3, text: 'Book', amount: -10},
+        {id: 4, text: 'Camera', amount: -399},
+      ]
+  
+      return {
+        transactions
+      }
+  }
+}
+</script>
